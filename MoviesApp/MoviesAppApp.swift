77809c1a8e348d630012636ac4225b1ab75bccaf
khaled-circle <k.bohout@trycircle.com>
2025-05-18 18:22:30 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Khaled-Circle on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
